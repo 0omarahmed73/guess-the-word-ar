7c88c3f2584d7941295da394524d1704bb9dfb38
Omar Ahmed <0omarahmed73@gmail.comd>
2024-09-08 17:08:42 +0300

    chore: Update Win component to reset 'youWin' state
    - Update the Win component to reset the 'youWin' state.
    - Add the 'vite-plugin-pwa' dependency to the package.json file.
    - Add the 'manifest.webmanifest' file to configure the PWA manifest.
    - Update the 'vite.config.js' file to include the VitePWA plugin and configure the PWA manifest.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+// PWA Manifest configuration
+const manifestForPlugIn = {
+  registerType: 'autoUpdate', // Automatically updates the service worker
+  injectRegister: 'auto', // Automatically injects the service worker registration code
+  includeAssets: ['draw_svg20210709-31688-va7bmk.svg.ico', 'masked-icon.png'],
+  manifest: {
+    name: 'Guess The Word',
+    short_name: 'Guess The Word',
+    description: 'I am a simple Vite app',
+    icons: [
+      {
+        src: '/draw_svg20210709-31688-va7bmk.svg.ico',
+        sizes: '256x256',
+        type: 'image/x-icon',
+        purpose: 'favicon',
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes: '256x256',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
+
+// Vite Configuration
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+});
